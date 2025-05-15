353e704fc92dd4db89782c5c0e7349821cf446bb
Nicolas <nico.hhara@gmail.com>
2025-05-15 12:22:03 -0300

    dasda

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ function renderTabelePeriodica(){
         let dadoColuna = '';
         for (let coluna = 1; coluna <= 18; coluna++) {
             dadoColuna = coluna+'TD';
+            dadoColuna.innerHTML = `
+        <div class="numero">1</div>
+        <div class="simbolo">H</div>
+        <div class="nome">Hidrogênio</div>
+        <div class="familia">Não-metal</div>
+        <div class="massa">1.008</div>
+        <div class="estado">Gasoso</div>`;
             console.log(dadoLinha+dadoColuna);    
         }
         // fecha tr
