0b5b916355c20a7d3a2623869e23e71cecedcfcf
Nicolas <nico.hhara@gmail.com>
2025-07-02 11:33:31 -0300

    asdas

--- tabela/script.js
+++ tabela/script.js
@@ -11,33 +11,24 @@ async function carregarDados() {
 }
 */
 
-//visor que aparece o Elemento
 let visorElemento;
 
-//array que armazena os produtos
 let asideArray = [];
 
-//container criado ao lado o Carrinho
 let asideContainer;
 
-
-//funcao para gerar a tabela Periodica
 function renderTabelaPeriodica(elementos) {
 
-    //cria uma table com a classe de 'tabela-periodica'
     const tabela = document.createElement('table');
-    tabela.classList.add('tabela-periodica');
+    tabela.classList.add('tabelaPeriodica');
 
     const main = document.querySelector('main');
 
-    //cria uma div com a funcao de visor e adiciona a classe 'visorElemento'
     visorElemento = document.createElement('div');
     visorElemento.classList.add('visorElemento');
 
-    //torna a variavel main pai do visor
     main.appendChild(visorElemento);
 
-    //cria um laco de repeticao para gerar as colunas e linhas da tabela
     for (let linha = 1; linha <= 10; linha++) {
         const tr = document.createElement('tr');
         tr.classList.add('tabelaLinha');
@@ -46,52 +37,42 @@ function renderTabelaPeriodica(elementos) {
             const td = document.createElement('td');
             td.classList.add('tabelaColuna');
 
-            //acha os elemntos na pasta do lado
             const elemento = elementos.find(
                 (el) => el.linha === linha && el.coluna === coluna
             );
             
-            //cria uma condicao
             if (elemento) {
-                //cria uma div, adiciona uma classe 'container' e da cor a ele
                 const div = document.createElement('div');
                 div.classList.add('container');
                 div.style.backgroundColor = elemento.corGrupo || '#FFFFFF';
 
-                //cria os elementos podendo arrastar
                 const innerDiv = document.createElement('div');
+                innerDiv.classList.add("containerElementos")
                 innerDiv.draggable = true;
                 innerDiv.ondragstart = dragstart_handler;
 
-                //adiona a div no HTML
                 innerDiv.innerHTML = `
-                    <div class="numero">${elemento.numeroAtomico}</div>
-                    <div class="simbolo">${elemento.simbolo}</div>
-                    <div class="nome">${elemento.nome}</div>
-                    <div class="massa">${elemento.massaAtomica}</div>
+                        <div class="elementoNumero">${elemento.numeroAtomico}</div>
+                        <div class="elementoSimbolo">${elemento.simbolo}</div>
+                        <div class="elementoNome">${elemento.nome}</div>
+                        <div class="elementoMassa">${elemento.massaAtomica}</div>
                 `;
 
-                //adiciona uma evento de click para a div arrastavel
                 innerDiv.addEventListener('click', () => {
                     mostrarDadosElemento(elemento);
                     adicionarAoAside(elemento);
                 });
 
-                //torna as divs 'innerDiv' filha da div 'div', e a 'div' filha da 'td'
                 div.appendChild(innerDiv);
                 td.appendChild(div);
             }
-            //'tr' vira pai da 'td'
             tr.appendChild(td);
         }
-        //'tabela' vira pai do 'tr'
         tabela.appendChild(tr);
     }
-    //'main' vira pai da 'tablela'
     main.appendChild(tabela);
 }
 
-//funcao de renderizar os elementos no aside
 function mostrarDadosElemento(elemento) {
     visorElemento.innerHTML = `
         <div class="containerTabela" style="background-color: ${elemento.corGrupo || '#FFFFFF'}">
@@ -103,7 +84,10 @@ function mostrarDadosElemento(elemento) {
     `;
 }
 
-//funcao para criar o aside
+function calcularElementos(elemento){
+    alert("SDADASDAD")
+}
+
 function criarAside() {
     asideContainer = document.createElement('aside');
     asideContainer.classList.add('aside');
@@ -122,7 +106,7 @@ function adicionarAoAside(elemento) {
     const container = document.getElementById('itensAside');
 
     const elementoAside = document.createElement('div');
-    elementoAside.classList.add('itemElemento');
+    elementoAside.classList.add('containerElementosAside');
     elementoAside.innerHTML = `<p>${elemento.simbolo} - ${elemento.nome}</p>`;
 
     asideArray.push(elementoAside);
